4e9f4bd5412a192fff91f6a26e332d6923793b12
wamwea <patokinya12@gmail.com>
2024-03-31 09:20:04 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution here
+function findMatching(names, query) {
+    return names.filter(
+      (possibleMatch) => possibleMatch.toLowerCase() === query.toLowerCase()
+    );
+  }
+  function findMatching(names, query) {
+    return names.filter(
+      (possibleMatch) => possibleMatch.toLowerCase() === query.toLowerCase()
+    );
+  }
+  
+  function fuzzyMatch(names, testString) {
+    return names.filter(
+      (possibleMatch) =>
+        possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+    );
+  }
+  
+  function matchName(source, soughtName) {
+    return source.filter((record) => record.name === soughtName);
+  }
+  function fuzzyMatch(source, testString) {
+    return source.filter(
+      (possibleMatch) =>
+        possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+    );
+  }
+  
+  function matchName(source, soughtName) {
+    return source.filter((record) => record.name === soughtName);
+  }
